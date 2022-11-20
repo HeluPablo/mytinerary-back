7eb6c24615dd0cf1c7a0e6f6fe7106f3ef09da94
HeluPablo <pablo_andaas@hotmail.com>
2022-11-20 19:19:32 -0300

    Update City.js

--- controllers/City.js
+++ controllers/City.js
@@ -16,5 +16,28 @@ const cityController = {
             });
         }
     },
+    readAll: async (req, res) => {
+        let query = {};
+        if (req.query.continent) {
+            query = { continent: req.query.continent };
+        }
+        if (req.query.name) {
+            query = {...query, name: { $regex: req.query.name, $options: "i" },
+            };
+        }
+        try {
+            let city = await City.find(query);
+            res.status(200).json({
+                response: city,
+                success: true,
+                message: "Cities found successfully",
+            });
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
 }
     module.exports = cityController;
