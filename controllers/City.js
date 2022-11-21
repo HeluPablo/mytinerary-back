1fabf4259f15cc77c0b56e66f87017eb949bcef4
HeluPablo <pablo_andaas@hotmail.com>
2022-11-21 09:50:15 -0300

    Update City.js

--- controllers/City.js
+++ controllers/City.js
@@ -66,5 +66,29 @@ const cityController = {
             });
         }
     },
+    update: async (req, res) => {
+        let id = req.params.id;
+        try {
+            let city = await City.findOneAndUpdate({ _id: id }, req.body, {new: true} )
+            
+            if (city) {
+                res.status(200).json({
+                    response: city,
+                    success: true,
+                    message: "City update successfully",
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: error.message,
+                });
+            }
+        } catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
 }
     module.exports = cityController;
