e85cfc768a7dd0d2189bda02ec4940ab6a2e5534
HeluPablo <pablo_andaas@hotmail.com>
2022-11-21 09:50:19 -0300

    Update cities.js

--- routes/cities.js
+++ routes/cities.js
@@ -1,9 +1,10 @@
 let router = require('express').Router();
-let { createCity , readAll , readOne} = require('../controllers/city');
+let { createCity , readAll , readOne, update} = require('../controllers/city');
 
 router.post('/', createCity);
 router.get('/', readAll);
 router.get('/:id', readOne);
+router.put("/:id",update)
 
 
 module.exports = router;
