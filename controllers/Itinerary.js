d2071e8a5361fba6a0e7c81436f6f75efa22bd32
HeluPablo <pablo_andaas@hotmail.com>
2022-12-01 04:19:36 -0300

    Update Itinerary.js

--- controllers/Itinerary.js
+++ controllers/Itinerary.js
@@ -1,101 +1,104 @@
-const Itinerary = require('../models/Itinerary');
-
-
+const Itinerary = require("../models/Itinerary");
 
 const controller = {
-    
-    read: async (req, res) => {
-        let query = {};
+  read: async (req, res) => {
+    let query = {};
 
+    if (req.query.cityId) {
+      query = {
+        cityId: req.query.cityId,
+      };
+      
+      if (req.query.userId) {
+        query = { userId: req.query.userId };
+      }
+    }
 
-        if (req.query.cityId) {
-            query = {
-                cityId: req.query.cityId
-            };
-        }
-        try {
-            let itineraries = await Itinerary.find(query);
-            if (itineraries) {
-                res.status(200).json({
-                    success: true,
-                    message: 'Itinerary find successfully',
-                    response: itineraries,
-                })
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: error.message,
-                });
-            }
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message,
-            });
-        }
-    },
-   create: async (req, res) => {
-        try {
-            let itinerary = await Itinerary.create(req.body);
-            res.status(201).json({
-                response: itinerary,
-                success: true,
-                message: "Itinerary created successfully",
-            });
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message,
-            });
-        }
-    },
-    update: async (req, res) => {
-        let id = req.params.id;
-        try {
-            let itinerary = await Itinerary.findOneAndUpdate({ _id: id }, req.body, {new: true} )
-            
-            if (itinerary) {
-                res.status(200).json({
-                    response: itinerary,
-                    success: true,
-                    message: "Itinerary update successfully",
-                });
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: error.message,
-                });
-            }
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message,
-            });
-        }
-    },
-    destroy: async (req, res) => {
-        let id = req.params.id;
-        try {
-            let itinerary = await Itinerary.findOneAndDelete({ _id: id }, req.body, )
-            
-            if (itinerary) {
-                res.status(200).json({
-                    response: itinerary._id,
-                    success: true,
-                    message: "Itinerary was deleted successfully",
-                });
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: error.message,
-                });
-            }
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message,
-            });
-        }
-    },
-}
+    try {
+      let itineraries = await Itinerary.find(query);
+      if (itineraries) {
+        res.status(200).json({
+          success: true,
+          message: "Itinerary find successfully",
+          response: itineraries,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: error.message,
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      let itinerary = await Itinerary.create(req.body);
+      res.status(201).json({
+        response: itinerary,
+        success: true,
+        message: "Itinerary created successfully",
+      });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  update: async (req, res) => {
+    let id = req.params.id;
+    try {
+      let itinerary = await Itinerary.findOneAndUpdate({ _id: id }, req.body, {
+        new: true,
+      });
+
+      if (itinerary) {
+        res.status(200).json({
+          response: itinerary,
+          success: true,
+          message: "Itinerary update successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: error.message,
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  destroy: async (req, res) => {
+    let id = req.params.id;
+    try {
+      let itinerary = await Itinerary.findOneAndDelete({ _id: id }, req.body);
+
+      if (itinerary) {
+        res.status(200).json({
+          response: itinerary._id,
+          success: true,
+          message: "Itinerary was deleted successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: error.message,
+        });
+      }
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+};
 module.exports = controller;
