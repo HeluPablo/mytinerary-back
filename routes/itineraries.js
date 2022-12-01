3e1d7d9348935a45ac6ca1426c26030dbf310c7b
HeluPablo <pablo_andaas@hotmail.com>
2022-12-01 06:17:42 -0300

    Update itineraries.js

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,10 +1,11 @@
-let router = require('express').Router();
-const { Router } = require('express');
-let { read , create, update, destroy } = require('../controllers/Itinerary');
+var express = require('express');
+var router = express.Router();
+let { read , create, update, destroy , readOne } = require('../controllers/Itinerary');
  
 router.get('/', read );
 router.post('/', create );
 router.put('/:id', update);
+router.get('/:id', readOne);
 router.delete('/:id', destroy);
 
 
